7748972a291b5de3d97c1773e727c4f92afa699d
crislimv2 <crislimpar123@gmail.com>
2025-07-02 01:13:27 +0700

    add sidebar alphabetical letter

--- components/Card.tsx
+++ components/Card.tsx
@@ -3,11 +3,11 @@ import CardRightPanel from "./CardRightPanel";
 
 const Card = () => {
   return (
-    <div className="bg-white w-full h-screen flex items-center justify-between">
-      <div className="bg-blue-200 w-3/4 h-full overflow-y-scroll">
+    <div className="bg-white h-screen w-full flex">
+      <div className="bg-blue-200 h-screen w-3/4 overflow-y-scroll">
         <CardLeftPanel />
       </div>
-      <div className="bg-yellow-200 w-1/4 h-screen sticky top-0">
+      <div className="bg-yellow-200 w-1/4 sticky top-0 h-screen overflow-y-scroll ">
         <CardRightPanel />
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Card from "../../components/Card"
 
 export default function Home() {
   return (
-    <div className="m-0 p-0 bg-[#0a0a0a] flex items-center justify-center h-screen w-screen">
+    <div className="m-0 p-0 bg-[#0a0a0a] h-screen w-screen">
       <Card/>
     </div>
   );
